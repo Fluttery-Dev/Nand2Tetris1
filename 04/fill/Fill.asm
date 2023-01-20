5d63fca575d39f0f6a5305718c4d6a7c0a7eb5f5
Lokesh Mishra <flutterydev@gmail.com>
2023-01-20 16:35:18 +0530

    Week 4 done

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,77 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//intitialise A with no of registers to blacken/whiten
+@8192
+D=A
+
+@n
+M=D
+
+
+
+//Infinite LOOP
+
+(LOOP)
+
+//Get keyboard inoput 
+@KBD
+D=M
+
+//check if pressed
+@PRESSED
+D;JGT
+
+//if not pressed set color to 0 and call fillcolor
+@color
+M=0
+@FILLCOLOR
+0;JMP
+
+//if pressed set color to -1 and call fillcolor
+(PRESSED)
+@color
+M=-1
+
+@FILLCOLOR
+0;JMP
+
+
+
+//sets n registers on screen to color
+(FILLCOLOR)
+//get screen coordinates
+@SCREEN
+D=A
+@temp
+M=D
+
+@i
+M=0
+
+//fill the registers
+(FILL)
+@i
+D=M
+
+@n
+D=M-D
+@LOOP
+D;JEQ
+
+@color
+D=M
+
+@temp
+M=M+1
+A=M-1
+M=D
+
+@i
+M=M+1
+@FILL
+0;JMP
+
+@LOOP
+0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -5,8 +5,41 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
+
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+
+
+@R1
+D=M
+@R2
+M=0
+
+// start of the loop
+(LOOP)
+@R0
+D = M
+
+@STOP
+D;JEQ
+
+@R1
+D =M
+
+@R2
+M = M+D
+
+@R0
+M = M-1
+
+@LOOP
+0;JMP
+
+(STOP)
+@STOP
+0;JMP
+
+
